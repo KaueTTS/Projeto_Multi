8a0b1d2bf0a3c45399728e6f409515fe857c5c0f
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-11-30 19:02:30 -0300

    Criando o gerador de senhas

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -50,3 +50,48 @@ function gerarArquivo() {
 
     clearInput()
 }
+
+// Password Generator 
+let inputSlider = document.getElementById("inputSlider")
+let sliderValue = document.getElementById("sliderValue")
+let passBox = document.getElementById("passBox")
+let lowercase = document.getElementById("lowercase")
+let uppercase = document.getElementById("uppercase")
+let numbers = document.getElementById("numbers")
+let symbols = document.getElementById("symbols")
+let passBtn = document.getElementById("passBtn")
+
+sliderValue.textContent = inputSlider.value
+inputSlider.addEventListener('input', ()=>{
+    sliderValue.textContent = inputSlider.value
+})
+
+passBtn.addEventListener('click', () => {
+    passBox.value = gerarSenha()
+})
+
+let lowerChars = "abdefghijklmnopqrstuvwxyz"
+let upperChars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let allNumbers = "0123456789"
+let allSymbols = "!@#$%&*"
+
+function gerarSenha() {
+    let genPassword = ""
+    let allChars = ""
+
+    allChars += lowercase.checked ? lowerChars : ""
+    allChars += uppercase.checked ? upperChars : ""
+    allChars += numbers.checked ? allNumbers : ""
+    allChars += symbols.checked ? allSymbols : ""
+
+    if (allChars == "" || allChars.length == 0) {
+        return genPassword
+    }
+
+    for (let i = 1; i <= inputSlider.value; i++) {
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length))
+    }
+
+    return genPassword
+
+}
